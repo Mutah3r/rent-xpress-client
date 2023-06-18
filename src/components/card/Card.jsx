e817b09b88df8d2aa9080be7014eb89686faf3fd
Mutah3r <evil.programmer.stephen@gmail.com>
2023-06-19 05:26:05 +0600

    fetch dynamic data from server and update Featured Cars section

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,27 +1,30 @@
-import { FaBriefcase, FaUser, FaCarAlt, FaCarSide, FaHeart } from 'react-icons/fa';
+import { FaBriefcase, FaUser, FaCarAlt, FaCarSide } from 'react-icons/fa';
 
-const Card = () => {
+const Card = ({ carInfo }) => {
+    const { title, price, specifications, images } = carInfo;
+    const { seats, luggage, doors, body } = specifications;
+
     return (
         <div className="border p-3 rounded-lg">
-            <div className="rounded-lg">
-                <img className="rounded-lg" src="https://images.unsplash.com/photo-1563721938524-4da1bede2935?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+            <div className="rounded-lg h-[250px] overflow-hidden">
+                <img className="rounded-lg w-full h-full transition-all duration-[0.5s] hover:scale-[1.05] cursor-pointer" src={images[0]} alt="" />
             </div>
             <div className="p-4">
                 <div className="flex justify-between">
-                    <h3 className="text-[18px] font-semibold title-font">Jeep Renegade</h3>
-                    <span className="text-[14px] flex items-center justify-center gap-1"><FaHeart className="text-[#CCCCCC]" /> 74</span>
+                    <h3 className="text-[18px] font-semibold title-font">{title}</h3>
+                    {/* <span className="text-[14px] flex items-center justify-center gap-1"><FaHeart className="text-[#CCCCCC]" /> 74</span> */}
                 </div>
                 <div className="mt-2 text-[14px] text-black font-semibold flex gap-3">
-                    <span className="flex items-center justify-center gap-1"><FaUser /> 5</span>
-                    <span className="flex items-center justify-center gap-1"><FaBriefcase /> 2</span>
-                    <span className="flex items-center justify-center gap-1"><FaCarSide /> 4</span>
-                    <span className="flex items-center justify-center gap-1"><FaCarAlt /> SUV</span>
+                    <span className="flex items-center justify-center gap-1"><FaUser /> {seats}</span>
+                    <span className="flex items-center justify-center gap-1"><FaBriefcase /> {luggage}</span>
+                    <span className="flex items-center justify-center gap-1"><FaCarSide /> {doors}</span>
+                    <span className="flex items-center justify-center gap-1"><FaCarAlt /> {body}</span>
                 </div>
                 <hr className="mt-3 mb-3" />
                 <div className="flex justify-between items-center">
                     <div>
                         <h4 className="text-[14px]">Daily rate from</h4>
-                        <h2 className="font-bold text-[26px]">$265</h2>
+                        <h2 className="font-bold text-[26px]">${price}</h2>
                     </div>
                     <div>
                         <button className="button-primary">Rent Now</button>
--- src/pages/home/FeaturedCars.jsx
+++ src/pages/home/FeaturedCars.jsx
@@ -7,6 +7,14 @@ import Card from "../../components/card/Card";
 import { useEffect, useState } from "react";
 
 const FeaturedCars = () => {
+    const [featuredCars, setFeaturedCars] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/cars")
+            .then(res => res.json())
+            .then(data => setFeaturedCars(data));
+    }, []);
+
     const getWindowSize = () => {
         const { innerWidth, innerHeight } = window;
         return { innerWidth, innerHeight };
@@ -26,29 +34,28 @@ const FeaturedCars = () => {
         };
     }, [])
 
-
-
     return (
         <section className="p-4">
             <h2 className="text-[42px] title-font text-[#3F4254] font-semibold text-center pt-6 pb-2 px-2">Our Vehicle Fleet</h2>
             <p className="pb-6 text-center px-2 text-[#3F4254] ">Driving your dreams to reality with an exquisite fleet of versatile vehicles for unforgettable journeys.</p>
 
             <Swiper
-                slidesPerView={windowSize.innerWidth > 800 ? 3 : windowSize.innerWidth > 550 ? 2 : 1}
+                slidesPerView={windowSize.innerWidth > 1000 ? 3 : windowSize.innerWidth > 650 ? 2 : 1}
                 spaceBetween={30}
                 freeMode={true}
                 pagination={{
                     clickable: true,
                 }}
                 modules={[FreeMode, Pagination]}
-                className="mySwiper"
+                className="mySwiper select-none"
             >
-                <SwiperSlide className="min-h-[50vh]"><Card /></SwiperSlide>
-                <SwiperSlide className="min-h-[50vh]"><Card /></SwiperSlide>
-                <SwiperSlide className="min-h-[50vh]"><Card /></SwiperSlide>
-                <SwiperSlide className="min-h-[50vh]"><Card /></SwiperSlide>
-                <SwiperSlide className="min-h-[50vh]"><Card /></SwiperSlide>
-                <SwiperSlide className="min-h-[50vh]"><Card /></SwiperSlide>
+                {
+                    featuredCars.map(car => {
+                        return (
+                            <SwiperSlide key={car._id} className="min-h-[50vh]"><Card carInfo={car} /></SwiperSlide>
+                        );
+                    })
+                }
             </Swiper>
         </section>
     );
