cb651b61a2c4235f33f421128c697ac0c772d35a
Mutah3r <evil.programmer.stephen@gmail.com>
2023-06-19 02:18:59 +0600

    add footer

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar";
+import Footer from "../pages/shared/Footer";
 
 const Main = () => {
     return (
         <div className="max-w-screen-2xl mx-auto">
             <Navbar />
             <Outlet />            
+            <Footer />
         </div>
     );
 };
