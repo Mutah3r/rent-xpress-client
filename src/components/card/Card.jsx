f52217bb2992a5a5b67eb01c300a08ff5fd949b3
Mutah3r <evil.programmer.stephen@gmail.com>
2023-06-21 07:26:50 +0600

    update Card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,11 +8,11 @@ const Card = ({ carInfo }) => {
     return (
         <div className="border border-gray-300 p-3 rounded-lg">
             <div className="rounded-lg h-[250px] overflow-hidden">
-                <img className="rounded-lg w-full h-full transition-all duration-[0.5s] hover:scale-[1.05] cursor-pointer" src={images[0]} alt="" />
+                <Link to={`/cars/${carInfo?._id ? carInfo._id : ""}`}><img className="rounded-lg w-full h-full transition-all duration-[0.5s] hover:scale-[1.05] cursor-pointer" src={images[0]} alt="" /></Link>
             </div>
             <div className="p-4">
                 <div className="flex justify-between">
-                    <h3 className="text-[18px] font-semibold title-font">{title}</h3>
+                    <Link to={`/cars/${carInfo?._id ? carInfo._id : ""}`}><h3 className="text-[18px] font-semibold title-font">{title}</h3></Link>
                     {/* <span className="text-[14px] flex items-center justify-center gap-1"><FaHeart className="text-[#CCCCCC]" /> 74</span> */}
                 </div>
                 <div className="mt-2 text-[14px] text-black font-semibold flex gap-3">
@@ -28,7 +28,7 @@ const Card = ({ carInfo }) => {
                         <h2 className="font-bold text-[26px]">${price}</h2>
                     </div>
                     <div>
-                        <Link to={`/cars/${carInfo?._id? carInfo._id : ""}`}><button className="button-primary">Rent Now</button></Link>
+                        <Link to={`/cars/${carInfo?._id ? carInfo._id : ""}`}><button className="button-primary">Rent Now</button></Link>
                     </div>
                 </div>
             </div>
